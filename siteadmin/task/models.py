31d441fdc9f57471acb4b261901183bfeca91659
simeialves <simeiparreiras@gmail.com>
2022-12-28 15:53:59 -0300

    first commit

---
+++ siteadmin/task/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=100)
+    category = models.TextField(max_length=50)
+    date_expiration = models.DateField(blank=True, null=True)
+
+    def __str__(self):
+        return self.title, self.date_expiration
